126c2d483da83658bd3428b8688866209ff51b2a
Vilma <vilmail@protonmail.com>
2023-06-15 21:25:26 +0300

    change page settings by input on start

--- src/config.js
+++ src/config.js
@@ -1,6 +1,10 @@
 import env from "dotenv";
 env.config();
 
-export const PORT = process.env.PORT;
+export const PORT = parseInt(process.env.PORT);
 export const NODE_ENV = process.env.NODE_ENV;
 export const DB_BASE_PATH = new URL("./../db/", import.meta.url).pathname;
+export const DEFAULT_ITEMS_PER_PAGE = parseInt(process.env.DEFAULT_ITEMS_PER_PAGE);
+export const MIN_ITEMS_PER_PAGE = parseInt(process.env.MIN_ITEMS_PER_PAGE);
+export const MAX_ITEMS_PER_PAGE = parseInt(process.env.MAX_ITEMS_PER_PAGE);
+export const CHANGE_PER_PAGE_BY_INPUT = process.env.CHANGE_PER_PAGE_BY_INPUT === "true" ? true : false;
---
+++ src/utils/configsByInput.js
@@ -0,0 +1,75 @@
+import readline from "node:readline/promises";
+import inventoryModel from "../models/inventoryModel.js";
+import { CHANGE_PER_PAGE_BY_INPUT } from "../config.js";
+
+let defaultItemsPerPage = process.env.DEFAULT_ITEMS_PER_PAGE;
+let minItemsPerPage = process.env.MIN_ITEMS_PER_PAGE;
+let maxItemsPerPage = process.env.MAX_ITEMS_PER_PAGE;
+
+if (CHANGE_PER_PAGE_BY_INPUT) {
+  const rl = readline.createInterface({ input: process.stdin, output: process.stdout });
+  const yes = { inputs: ["y", "Y", "yes", "Yes", "YES"], value: true };
+  const no = { inputs: ["n", "N", "no", "No", "NO"], value: false };
+  const quit = { inputs: ["q", "Q", "quit", "Quit", "QUIT"], action: rl.close };
+
+  const checkIfToUseDefaults = async () => {
+    const useDefaults = await rl.question(`
+Current configuration for
+DEFAULT_ITEMS_PER_PAGE are: ${defaultItemsPerPage},
+MIN_ITEMS_PER_PAGE: ${minItemsPerPage},
+MAX_ITEMS_PER_PAGE: ${maxItemsPerPage}.
+You can change these settings permanently by changing ENV
+You can turn off these prompts by setting env CHANGE_PER_PAGE_BY_INPUT to false;
+Do You want to change these settings temporary? (y/n)
+`);
+    if (yes.inputs.includes(useDefaults)) {
+      return yes.value;
+    }
+    if (no.inputs.includes(useDefaults)) {
+      return no.value;
+    }
+    return checkIfToUseDefaults();
+  };
+
+  const needsChangingDefaults = await checkIfToUseDefaults();
+
+  if (needsChangingDefaults) {
+    const allRecordsCount = await inventoryModel.getCount();
+    const getDefaultPageSize = async (msg = "Enter default items count per page") => {
+      let defaultSize = await rl.question(msg + ":");
+      defaultSize = Math.round(Number(defaultSize));
+      if (!isNaN(defaultSize) && defaultSize > 0 && defaultSize <= allRecordsCount) {
+        return defaultSize;
+      }
+      return getDefaultPageSize(`${msg} as number(1-${allRecordsCount})`);
+    };
+
+    const getMinPageSize = async (msg = "Enter minimum items count per page") => {
+      let minSize = await rl.question(msg + ":");
+      minSize = Math.round(Number(minSize));
+      if (!isNaN(minSize) && minSize <= allRecordsCount && minSize > 0) {
+        return minSize;
+      }
+      return getMinPageSize(`${msg} as number(1-${allRecordsCount})`);
+    };
+
+    const getMaxPageSize = async (msg = "Enter maximum items count per page") => {
+      let maxSize = await rl.question(msg + ":");
+      maxSize = Math.round(Number(maxSize));
+      if (!isNaN(maxSize) && maxSize <= allRecordsCount && maxSize > 0) {
+        return maxSize;
+      }
+      return getMaxPageSize(`${msg} as number(1-${allRecordsCount})`);
+    };
+
+    defaultItemsPerPage = await getDefaultPageSize();
+    minItemsPerPage = await getMinPageSize();
+    maxItemsPerPage = await getMaxPageSize();
+  }
+  console.log(`Current configuration for
+DEFAULT ITEMS PER PAGE are: ${defaultItemsPerPage},
+MIN ITEMS PER PAGE: ${minItemsPerPage},
+MAX ITEMS PER PAGE: ${maxItemsPerPage}.`);
+}
+
+export { defaultItemsPerPage, minItemsPerPage, maxItemsPerPage };
